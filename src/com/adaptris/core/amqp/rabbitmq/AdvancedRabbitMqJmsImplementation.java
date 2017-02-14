295cc4e01edd25a704626482de8ad9ab16c68c7c
aaron-mcgrath-adp <aaron.mcgrath@adaptris.com>
2017-02-14 15:06:41 +0000

    INTERLOK-1142 Added RabbitMQ implementations to the existing AMQP project - basic tests performed, needs a little cleanup and unit tests.

---
+++ src/com/adaptris/core/amqp/rabbitmq/AdvancedRabbitMqJmsImplementation.java
@@ -0,0 +1,200 @@
+package com.adaptris.core.amqp.rabbitmq;
+
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.validation.constraints.NotNull;
+
+import com.adaptris.annotation.AutoPopulated;
+import com.adaptris.core.CoreException;
+import com.adaptris.core.jms.JmsConnection;
+import com.adaptris.core.jms.VendorImplementation;
+import com.adaptris.core.jms.VendorImplementationBase;
+import com.adaptris.security.exc.PasswordException;
+import com.adaptris.util.KeyValuePair;
+import com.adaptris.util.KeyValuePairSet;
+import com.rabbitmq.jms.admin.RMQConnectionFactory;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+
+/**
+ * AMQP 1.0 implementation of {@link VendorImplementation} using RabbitMQ.
+ * 
+ * <p>
+ * All available connection factory properties are exposed. If you
+ * configure a username and password on the wrapping {@link JmsConnection} in which case
+ * {@link ConnectionFactory#createConnection(String, String)} is used when creating the connection
+ * otherwise {@link ConnectionFactory#createConnection()} will be used.
+ * </p>
+ * <p>
+ * This was built against {@code com.rabbitmq.jms:rabbitmq-jms:1.6.0} and {@code com.rabbitmq:amqp-client:4.0.2} 
+ * </p>
+ * 
+ * @config rabbitmq-advanced-jms-implementation
+ * @since 3.6.0
+ */
+@XStreamAlias("rabbitmq-advanced-jms-implementation")
+public class AdvancedRabbitMqJmsImplementation extends BasicRabbitMqJmsImplementation {
+  
+  @NotNull
+  @AutoPopulated
+  private KeyValuePairSet connectionFactoryProperties;
+  
+  private enum ConnectionFactoryProperties {
+    
+    Channel_QoS {
+      @Override
+      public void apply(RMQConnectionFactory connectionFactory, String value) throws CoreException {
+        connectionFactory.setChannelsQos(Integer.parseInt(value));
+      }
+    } ,
+    
+    Host {
+      @Override
+      public void apply(RMQConnectionFactory connectionFactory, String value) throws CoreException {
+        connectionFactory.setHost(value);
+      }
+    } ,
+    
+    OnMessageTimeoutMs {
+      @Override
+      public void apply(RMQConnectionFactory connectionFactory, String value) throws CoreException {
+        connectionFactory.setOnMessageTimeoutMs(Integer.parseInt(value));
+      }
+    } ,
+   
+    Password {
+      @Override
+      public void apply(RMQConnectionFactory connectionFactory, String value) throws CoreException {
+        try {
+          connectionFactory.setPassword(com.adaptris.security.password.Password.decode(value));
+        } catch (PasswordException e) {
+          throw new CoreException(e);
+        }
+      }
+    } ,
+    
+    Port {
+      @Override
+      public void apply(RMQConnectionFactory connectionFactory, String value) throws CoreException {
+        connectionFactory.setPort(Integer.parseInt(value));
+      }
+    } ,
+    
+    QueueBrowserReadMax {
+      @Override
+      public void apply(RMQConnectionFactory connectionFactory, String value) throws CoreException {
+        connectionFactory.setQueueBrowserReadMax(Integer.parseInt(value));
+      }
+    } ,
+    
+    Ssl {
+      @Override
+      public void apply(RMQConnectionFactory connectionFactory, String value) throws CoreException {
+        if(Boolean.getBoolean(value))
+          try {
+            connectionFactory.useSslProtocol();
+          } catch (NoSuchAlgorithmException e) {
+            throw new CoreException(e);
+          }
+      }
+    } ,
+    
+    UseSslProtocol {
+      @Override
+      public void apply(RMQConnectionFactory connectionFactory, String value) throws CoreException {
+        connectionFactory.useSslProtocol(value);
+      }
+    } ,
+    
+    TerminationTimeout {
+      @Override
+      public void apply(RMQConnectionFactory connectionFactory, String value) throws CoreException {
+        connectionFactory.setTerminationTimeout(Long.parseLong(value));
+      }
+    } ,
+    
+    TrustedPackages {
+      @Override
+      public void apply(RMQConnectionFactory connectionFactory, String value) throws CoreException {
+        connectionFactory.setTrustedPackages(Arrays.asList(value.split(",")));
+      }
+    } ,
+    
+    Uri {
+      @Override
+      public void apply(RMQConnectionFactory connectionFactory, String value) throws CoreException {
+        try {
+          connectionFactory.setUri(value);
+        } catch (JMSException e) {
+          throw new CoreException(e);
+        }
+      }
+    } ,
+    
+    UseDefaultSslContext {
+      @Override
+      public void apply(RMQConnectionFactory connectionFactory, String value) throws CoreException {
+        connectionFactory.setUseDefaultSslContext(Boolean.getBoolean(value));
+      }
+    } ,
+    
+    Username {
+      @Override
+      public void apply(RMQConnectionFactory connectionFactory, String value) throws CoreException {
+        connectionFactory.setUsername(value);
+      }
+    } ,
+    
+    VirtualHost {
+      @Override
+      public void apply(RMQConnectionFactory connectionFactory, String value) throws CoreException {
+        connectionFactory.setVirtualHost(value);
+      }
+    } ;
+    
+    public abstract void apply(RMQConnectionFactory connectionFactory, String value) throws CoreException;
+  }
+  
+  public AdvancedRabbitMqJmsImplementation() {
+    setConnectionFactoryProperties(new KeyValuePairSet());
+  }
+  
+  @Override
+  public ConnectionFactory createConnectionFactory() throws JMSException {
+    RMQConnectionFactory connectionFactory = new RMQConnectionFactory();
+    
+    try {
+      this.applyConnectionFactoryProperties(connectionFactory, this.getConnectionFactoryProperties());
+    } catch (CoreException e) {
+      throw new JMSException(e.getMessage());
+    }
+    
+    return connectionFactory;
+  }
+
+  private void applyConnectionFactoryProperties(RMQConnectionFactory connectionFactory, KeyValuePairSet connectionFactoryProperties2) throws CoreException {
+    for (KeyValuePair kvp : this.getConnectionFactoryProperties().getKeyValuePairs()) {
+      try {
+        ConnectionFactoryProperties.valueOf(kvp.getKey()).apply(connectionFactory, kvp.getValue());
+      } catch(Exception ex) {
+        throw new CoreException(ex);
+      }
+    }
+  }
+
+  @Override
+  public boolean connectionEquals(VendorImplementationBase vendorImp) {
+    return (vendorImp instanceof BasicRabbitMqJmsImplementation) && super.connectionEquals(vendorImp);
+  }
+
+  public KeyValuePairSet getConnectionFactoryProperties() {
+    return connectionFactoryProperties;
+  }
+
+  public void setConnectionFactoryProperties(KeyValuePairSet connectionFactoryProperties) {
+    this.connectionFactoryProperties = connectionFactoryProperties;
+  }
+
+}
---
+++ src/com/adaptris/core/amqp/rabbitmq/BasicRabbitMqJmsImplementation.java
@@ -0,0 +1,44 @@
+package com.adaptris.core.amqp.rabbitmq;
+
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+
+import com.adaptris.core.jms.JmsConnection;
+import com.adaptris.core.jms.UrlVendorImplementation;
+import com.adaptris.core.jms.VendorImplementation;
+import com.adaptris.core.jms.VendorImplementationBase;
+import com.rabbitmq.jms.admin.RMQConnectionFactory;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+
+/**
+ * AMQP 1.0 implementation of {@link VendorImplementation} using RabbitMQ.
+ * 
+ * <p>
+ * Everything required to configure the connection needs to be specified on the URL. If you
+ * configure a username and password on the wrapping {@link JmsConnection} in which case
+ * {@link ConnectionFactory#createConnection(String, String)} is used when creating the connection
+ * otherwise {@link ConnectionFactory#createConnection()} will be used.
+ * </p>
+ * <p>
+ * This was built against {@code com.rabbitmq.jms:rabbitmq-jms:1.6.0} and {@code com.rabbitmq:amqp-client:4.0.2} 
+ * </p>
+ * 
+ * @config rabbitmq-basic-jms-implementation
+ * @since 3.6.0
+ */
+@XStreamAlias("rabbitmq-basic-jms-implementation")
+public class BasicRabbitMqJmsImplementation extends UrlVendorImplementation {
+
+  @Override
+  public ConnectionFactory createConnectionFactory() throws JMSException {
+    RMQConnectionFactory connectionFactory;
+    connectionFactory = new RMQConnectionFactory();
+    connectionFactory.setUri(this.getBrokerUrl());
+    return connectionFactory;
+  }
+
+  @Override
+  public boolean connectionEquals(VendorImplementationBase vendorImp) {
+    return (vendorImp instanceof BasicRabbitMqJmsImplementation) && super.connectionEquals(vendorImp);
+  }
+}
