24adf0a87a51407be76e571f9b8aa723592ecdca
Lewin Chan <lewin.chan@adaptris.com>
2019-10-28 13:31:18 +0000

    Remove refs to package.svg since they are auto-generated now.

--- src/main/java/com/adaptris/core/amqp/qpid/package-info.java
+++ src/main/java/com/adaptris/core/amqp/qpid/package-info.java
@@ -1,6 +1,5 @@
 /**
  * AMQP 1.0 implementation using <a href="http://qpid.apache.org/index.html">Apache Qpid</a> as a JMS/AMQP bridge.
  * 
- * <img alt="UML" src="package.svg"/>
  */
 package com.adaptris.core.amqp.qpid;
--- src/main/java/com/adaptris/core/amqp/rabbitmq/package-info.java
+++ src/main/java/com/adaptris/core/amqp/rabbitmq/package-info.java
@@ -1,6 +1,4 @@
 /**
  * RabbitMQ JMS specific implementations.
- * 
- * <img alt="UML" src="package.svg"/>
  */
 package com.adaptris.core.amqp.rabbitmq;
